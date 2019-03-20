56d162799310c82217f8c670fc6731c027994a01
Roman <pomah156@gmail.com>
2019-03-20 11:07:34 +0300

    push to github

--- server/config.js
+++ server/config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production';
-const pathToEnv = path.join(__dirname, isProd ? '../' : '', '../.env');
-console.log(pathToEnv);
 require('dotenv').config({
-  silent: isProd,
-  path: pathToEnv,
+  silent: process.env.NODE_ENV === 'production',
+  path: path.join(__dirname, '../.env'),
 });
